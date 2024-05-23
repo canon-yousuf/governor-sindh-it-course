afa9f8b6b403dd7807e8e6570fbad8f4a1586321
Canon Yousuf <164149072+canon-yousuf@users.noreply.github.com>
2024-05-23 11:37:59 +0500

    Fifth Project

---
+++ learning-typescript/node_projects/Currency Converter/README.md
@@ -0,0 +1,21 @@
+# Currency Converter in TypeScript
+
+A simple Currency Converter application built with TypeScript. This is my fifth project, demonstrating currency conversion between USD, INR, and PKR in a command-line interface.
+
+## Table of Contents
+
+- [Features](#features)
+- [Usage](#usage)
+
+## Features
+
+- **Convert USD to INR or PKR**: Convert United States Dollar (USD) to Indian Rupee (INR) or Pakistani Rupee (PKR).
+- **Convert INR to USD or PKR**: Convert Indian Rupee (INR) to United States Dollar (USD) or Pakistani Rupee (PKR).
+- **Convert PKR to USD or INR**: Convert Pakistani Rupee (PKR) to United States Dollar (USD) or Indian Rupee (INR).
+
+## Usage
+
+To start the Currency Converter application, use the following command:
+
+```sh
+npx canoncurrencyconverter
---
+++ learning-typescript/node_projects/Currency Converter/index.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer"
+
+let conversion = {
+    "PKR": {
+        "USD": 0.0036,
+        "INR": 0.297,
+        "PKR": 1
+    },
+    "INR": {
+        "USD": 0.012,
+        "INR": 1,
+        "PKR": 3.367  
+    },
+    "USD": {
+        "USD": 1,
+        "INR": 83.3299,
+        "PKR": 280.5736
+    }
+}
+
+const answer: {
+    from: "PKR" | "INR" | "USD",
+    to: "PKR" | "INR" | "USD",
+    amount: number,
+} = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "INR", "USD"],
+        message: "Select your Currency: "
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "INR", "USD"],
+        message: "Select a Currency to Convert: "
+    },
+    {
+        type: "number",
+        name: "amount",
+        choices: ["PKR", "INR", "USD"],
+        message: "Enter Your Conversion Amount: "
+    }
+]);
+
+const {from, to, amount} = answer;
+
+if (from && to && amount) {
+    let result = conversion[from][to] * amount
+    console.log(`Your Conversion Currency ${from} to ${to} is ${result}`)
+} else {
+    console.log("Invalid Inputs")
+}
